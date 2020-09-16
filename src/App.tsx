0ff5f49b9b3500740fd572750ac0c37e90985c28
Danielle Man <man.danielleh@gmail.com>
2020-09-15 21:14:15 -0700

    use levels as the y-axis

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Chart } from './Chart';
+import React, { useState } from "react";
+import { Chart } from "./Chart";
 
 /**
  * First column dictates the y-axis, and is usually used
@@ -13,12 +13,18 @@ import { Chart } from './Chart';
  * First row of the CSV is reserved for labels, and is not
  * interpreted as data to plot; rather the labels for that data.
  */
-const sampleLabels = ['percentile', '1', '2', '3', '4', '5'];
-const sampleData = [
+const sampleGraphLabels = ["percentile", "L1", "L2", "L3", "L4", "L5"];
+const sampleGraphData = [
   [2, 100000, 110000, 120000, 130000, 140000],
   [4, 110000, 120000, 130000, 140000, 150000],
   [6, 120000, 130000, 140000, 150000, 160000],
   [8, 130000, 140000, 150000, 160000, 170000],
+];
+
+const sampleDataLabels = ["name", "2016", "2017", "2018", "2019", "2020"];
+const sampleDataPoints = [
+  [1, 100000, 110000, 120000, 130000, 140000, 150000],
+  [2, 200000, 210000, 220000, 230000, 240000, 250000],
 ];
 
 export interface LabelMap {
@@ -28,61 +34,35 @@ export interface LabelMap {
 export interface Point {
   x: number;
   y: number;
-  color: number;
   label: string;
 }
 
 export interface Line {
+  y: number;
   points: number[][]; // [percentile, comp]
-  color: number;
 }
 
-function App() {
+const DropZone: React.FC<{
+  title: string;
+  style?: object;
+  update: (fileContents: string) => void;
+}> = ({ title, style = {}, update }) => {
   const [isDraggedOver, setIsDraggedOver] = useState(false);
-  const [labels, setLabels] = useState<string[]>(sampleLabels);
-  const [data, setData] = useState<number[][]>(sampleData);
-
-  const labelMap: LabelMap = {};
-  labels.forEach((l, i) => {
-    if (i !== 0) {
-      labelMap[i] = l;
-    }
-  });
-
-  const linesCache: { [percentile: number]: number[][] } = {};
-  const pointsToPlot: Point[] = [];
-  data.forEach((row, i) => {
-    row.forEach((point, j) => {
-      if (j !== 0) {
-        pointsToPlot.push({
-          x: point,
-          y: row[0],
-          color: j,
-          // label: `${row[0]}th percentile ${labels[j]
-          //   }: $${point.toLocaleString()}`,
-          label: `x ${point.toLocaleString()} –– y ${row[0]} –– z ${labels[j]}`,
-        });
-
-        const pointsInLine = linesCache[j] || [];
-        pointsInLine.push([point, row[0]]); // x, y: percentile, comp
-        linesCache[j] = pointsInLine;
-      }
-    });
-  });
-
-  const linesToPlot = Object.entries(linesCache).map(([color, points]) => ({
-    color: parseInt(color),
-    points,
-  }));
 
   return (
     <div
       style={{
-        width: 700,
-        height: 300,
-        margin: '40px auto',
-        outline: isDraggedOver ? '4px dashed orange' : '4px solid transparent',
-        backgroundColor: isDraggedOver ? '#fafafa' : 'transparent',
+        ...style,
+        height: 100,
+        padding: 10,
+        flex: 1,
+        border: "1px solid #eee",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        outline: isDraggedOver ? "4px dashed orange" : "4px solid transparent",
+        backgroundColor: isDraggedOver ? "#fafafa" : "transparent",
       }}
       onDragOver={(e) => {
         e.preventDefault();
@@ -108,34 +88,112 @@ function App() {
           const file = event.dataTransfer.files[0];
           console.log(file);
 
-          if (file.type !== 'text/csv') {
-            console.log('Cannot process this file.');
+          if (file.type !== "text/csv") {
+            console.log("Cannot process this file.");
             return;
           }
 
           const reader = new FileReader();
-          reader.addEventListener('load', function handleFileLoad(e) {
+          reader.addEventListener("load", function handleFileLoad(e) {
             const data = e.target?.result;
-            if (typeof data === 'string') {
-              const rows = data.split('\n');
-              setLabels(rows[0].split(','));
-              setData(
-                rows
-                  .splice(1)
-                  .map((str) => str.split(',').map((i) => parseInt(i)))
-              );
-            }
-            reader.removeEventListener('load', handleFileLoad);
+            if (typeof data === "string") update(data);
+            reader.removeEventListener("load", handleFileLoad);
           });
           reader.readAsText(file);
         }
       }}
     >
-      <Chart
-        pointsToPlot={pointsToPlot}
-        linesToPlot={linesToPlot}
-        labelMap={labelMap}
-      />
+      <span role="img" aria-label="open mailbox" style={{ fontSize: 48 }}>
+        {isDraggedOver ? "📬" : "📭"}
+      </span>
+      <span style={{ fontSize: 12 }}>{title}</span>
+    </div>
+  );
+};
+
+function App() {
+  const [graphlabels, setGraphLabels] = useState<string[]>(sampleGraphLabels);
+  const [graphData, setGraphData] = useState<number[][]>(sampleGraphData);
+  const [pointLabels, setPointLabels] = useState<string[]>(sampleDataLabels);
+  const [pointData, setPointData] = useState<number[][]>(sampleDataPoints);
+
+  const labelMap: LabelMap = {};
+  graphlabels.forEach((l, i) => {
+    if (i !== 0) {
+      labelMap[i] = l;
+    }
+  });
+
+  const linesCache: { [percentile: number]: number[][] } = {};
+  const pointsToPlot: Point[] = [];
+  graphData.forEach((row, i) => {
+    row.forEach((point, j) => {
+      if (j !== 0) {
+        pointsToPlot.push({
+          x: point,
+          y: j,
+          label: `${graphlabels[j]}: ${
+            row[0]
+          }th percentile, ${point.toLocaleString()}`,
+        });
+
+        const pointsInLine = linesCache[j] || [];
+        pointsInLine.push([point, row[0]]); // x, y: percentile, comp
+        linesCache[j] = pointsInLine;
+      }
+    });
+  });
+
+  const linesToPlot = Object.entries(linesCache).map(([y, points]) => ({
+    y: parseInt(y),
+    points,
+  }));
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        marginTop: 20,
+        width: 900,
+        height: 300,
+        margin: "40px auto",
+      }}
+    >
+      <div style={{ flex: 1, marginRight: 20 }}>
+        <Chart
+          datumsToPlot={pointData}
+          pointsToPlot={pointsToPlot}
+          linesToPlot={linesToPlot}
+          labelMap={labelMap}
+        />
+      </div>
+      <div>
+        <DropZone
+          title="Drag in Graph Data"
+          style={{ marginBottom: 20 }}
+          update={(data) => {
+            const rows = data.split("\n");
+            setGraphLabels(rows[0].split(","));
+            setGraphData(
+              rows
+                .splice(1)
+                .map((str) => str.split(",").map((i) => parseInt(i)))
+            );
+          }}
+        />
+        {/* <DropZone
+          title="Upload Data Points"
+          update={(data) => {
+            const rows = data.split("\n");
+            setPointLabels(rows[0].split(","));
+            setPointData(
+              rows
+                .splice(1)
+                .map((str) => str.split(",").map((i) => parseInt(i)))
+            );
+          }}
+        /> */}
+      </div>
     </div>
   );
 }
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react';
-import { Point, Line, LabelMap } from './App';
-import * as d3 from 'd3';
+import React, { useRef, useEffect } from "react";
+import { Point, Line, LabelMap } from "./App";
+import * as d3 from "d3";
 
 const PADDING = 20; // pixels
 
@@ -16,14 +16,14 @@ const getGrid = (min: number, max: number, delimiter: number) =>
   );
 
 export const Chart: React.FC<{
+  datumsToPlot: number[][];
   pointsToPlot: Point[];
   linesToPlot: Line[];
   labelMap: LabelMap;
-}> = ({ pointsToPlot, linesToPlot, labelMap }) => {
+}> = ({ datumsToPlot, pointsToPlot, linesToPlot, labelMap }) => {
   const d3Container = useRef(null);
-  console.log(labelMap);
 
-  // console.log(pointsToPlot, linesToPlot, labelMap)
+  const line1 = datumsToPlot[0].slice(1);
 
   /**
    * The useEffect hooks is running side effects outside of React,
@@ -35,22 +35,24 @@ export const Chart: React.FC<{
         const svg = d3.select(d3Container.current);
 
         // color source: https://www.materialui.co/colors
-        const colors = [
-          '#e53935',
-          '#FB8C00',
-          '#FFEB3B',
-          '#8BC34A',
-          '#03A9F4',
-          '#BA68C8',
+        // const colors = [
+        //   "#e53935",
+        //   "#FB8C00",
+        //   "#FFEB3B",
+        //   "#8BC34A",
+        //   "#03A9F4",
+        //   "#BA68C8",
+        // ];
+        // var color = d3
+        //   .scaleLinear()
+        //   .domain(linesToPlot.map((d) => d.color))
+        //   // @ts-ignore
+        //   .range(colors);
+
+        const [dxMin, dxMax] = [
+          Math.min(...pointsToPlot.map((p) => p.x)),
+          Math.max(...pointsToPlot.map((p) => p.x)),
         ];
-        var color = d3
-          .scaleLinear()
-          .domain(linesToPlot.map((d) => d.color))
-          // @ts-ignore
-          .range(colors);
-
-
-        const [dxMin, dxMax] = [Math.min(...pointsToPlot.map((p) => p.x)), Math.max(...pointsToPlot.map((p) => p.x))]
         const [xMin, xMax] = [
           dxMin - (dxMax - dxMin) / 10,
           dxMax + (dxMax - dxMin) / 10,
@@ -61,174 +63,161 @@ export const Chart: React.FC<{
           // @ts-ignore
           .range([1.5 * PADDING, d3Container.current.clientWidth - PADDING]);
 
-        const [dyMin, dyMax] = [Math.min(...pointsToPlot.map((p) => p.y)), Math.max(...pointsToPlot.map((p) => p.y))]
-        const yStep = (dyMax - dyMin) / 8;
-        const [yMin, yMax] = [
-          dyMin - yStep,
-          dyMax + yStep,
+        const [dyMin, dyMax] = [
+          Math.min(...Object.keys(labelMap).map((k) => parseInt(k))),
+          Math.max(...Object.keys(labelMap).map((k) => parseInt(k))),
         ];
+        const yStep = 1;
+        const [yMin, yMax] = [dyMin - yStep, dyMax + yStep];
         const y = d3
           .scaleLinear()
           .domain([yMax, yMin])
           // @ts-ignore
           .range([PADDING, d3Container.current.clientHeight - 1.5 * PADDING]);
-        console.log(y(50))
 
         // x-axis grid
-        if (!svg.selectAll('g.x-axis').size()) svg.append('g').attr('class', 'x-axis');
-        const xAxisGroup = svg.select('g.x-axis');
-        const xLines = xAxisGroup.selectAll('line').data(getGrid(xMin, xMax, 10000));;
-        const xLabels = xAxisGroup.selectAll('text').data(getGrid(xMin, xMax, 10000));;
+        if (!svg.selectAll("g.x-axis").size())
+          svg.append("g").attr("class", "x-axis");
+        const xAxisGroup = svg.select("g.x-axis");
+        const xLines = xAxisGroup
+          .selectAll("line")
+          .data(getGrid(xMin, xMax, 10000));
+        const xLabels = xAxisGroup
+          .selectAll("text")
+          .data(getGrid(xMin, xMax, 10000));
         xLines
           .enter()
-          .append('line')
-          .attr('stroke', '#eee')
-          .attr('stroke-dasharray', 2)
-          .attr('x1', (d) => x(d))
-          .attr('x2', (d) => x(d))
-          .attr('y1', (d) => y(yMin))
-          .attr('y2', (d) => y(yMax));
+          .append("line")
+          .attr("stroke", "#eee")
+          .attr("stroke-dasharray", 2)
+          .attr("x1", (d) => x(d))
+          .attr("x2", (d) => x(d))
+          .attr("y1", (d) => y(yMin))
+          .attr("y2", (d) => y(yMax));
         xLines
-          .attr('x1', (d) => x(d))
-          .attr('x2', (d) => x(d))
-          .attr('y1', (d) => y(yMin))
-          .attr('y2', (d) => y(yMax))
+          .attr("x1", (d) => x(d))
+          .attr("x2", (d) => x(d))
+          .attr("y1", (d) => y(yMin))
+          .attr("y2", (d) => y(yMax));
         xLabels
           .enter()
-          .append('text')
-          .attr('font-size', 8)
-          .attr('fill', '#aaa')
-          .attr('text-anchor', 'middle')
-          .attr('alignment-baseline', 'hanging')
-          .attr('x', (d) => x(d))
-          .attr('y', (d) => y(yMin) + 6)
+          .append("text")
+          .attr("font-size", 8)
+          .attr("fill", "#aaa")
+          .attr("text-anchor", "middle")
+          .attr("alignment-baseline", "hanging")
+          .attr("x", (d) => x(d))
+          .attr("y", (d) => y(yMin) + 6)
           .text((d) => `${d / 1000}k`);
         xLabels
-          .attr('x', (d) => x(d))
-          .attr('y', (d) => y(yMin) + 6)
+          .attr("x", (d) => x(d))
+          .attr("y", (d) => y(yMin) + 6)
           .text((d) => `${d / 1000}k`);
         xLines.exit().remove();
         xLabels.exit().remove();
 
         // y-axis grid
-        if (!svg.selectAll('g.y-axis').size()) svg.append('g').attr('class', 'y-axis');
-        const yAxisGroup = svg.select('g.y-axis');
-        const yLines = yAxisGroup.selectAll('line').data(getGrid(yMin, yMax, yStep));;
-        const yLabels = yAxisGroup.selectAll('text').data(getGrid(yMin, yMax, yStep));;
+        if (!svg.selectAll("g.y-axis").size())
+          svg.append("g").attr("class", "y-axis");
+        const yAxisGroup = svg.select("g.y-axis");
+        const yLines = yAxisGroup
+          .selectAll("line")
+          .data(getGrid(yMin, yMax, yStep));
+        const yLabels = yAxisGroup
+          .selectAll("text")
+          .data(getGrid(yMin, yMax, yStep));
         yLines
           .enter()
-          .append('line')
-          .attr('stroke', '#eee')
-          .attr('stroke-dasharray', 2)
-          .attr('x1', x(xMin))
-          .attr('x2', (d) => x(xMax))
-          .attr('y1', (d) => y(d))
-          .attr('y2', (d) => y(d))
+          .append("line")
+          .attr("stroke", "#eee")
+          .attr("stroke-dasharray", 2)
+          .attr("x1", x(xMin))
+          .attr("x2", (d) => x(xMax))
+          .attr("y1", (d) => y(d))
+          .attr("y2", (d) => y(d));
         yLines
-          .attr('x1', x(xMin))
-          .attr('x2', (d) => x(xMax))
-          .attr('y1', (d) => y(d))
-          .attr('y2', (d) => y(d))
+          .attr("x1", x(xMin))
+          .attr("x2", (d) => x(xMax))
+          .attr("y1", (d) => y(d))
+          .attr("y2", (d) => y(d));
         yLabels
           .enter()
-          .append('text')
-          .attr('font-size', 8)
-          .attr('fill', '#aaa')
-          .attr('text-anchor', 'end')
-          .attr('alignment-baseline', 'middle')
-          .attr('x', x(xMin) - 6)
-          .attr('y', (d) => y(d))
-          .text((d) => d)
+          .append("text")
+          .attr("font-size", 8)
+          .attr("fill", "#aaa")
+          .attr("text-anchor", "end")
+          .attr("alignment-baseline", "middle")
+          .attr("x", x(xMin) - 6)
+          .attr("y", (d) => y(d))
+          .text((d) => labelMap[d] || "");
         yLabels
-          .attr('x', x(xMin) - 6)
-          .attr('y', (d) => y(d))
-          .text((d) => d);
+          .attr("x", x(xMin) - 6)
+          .attr("y", (d) => y(d))
+          .text((d) => labelMap[d] || "");
         yLines.exit().remove();
         yLabels.exit().remove();
 
-        // key groups
-        if (!svg.selectAll('g.key').size()) svg.append('g').attr('class', 'key');
-        const keyGroup = svg.select('g.key');
-        const labelArr = Object.entries(labelMap).map(([c, l]) => ({
-          color: parseInt(c),
-          label: l,
-        }));
-        console.log(labelArr);
-        const keyRects = keyGroup.selectAll('rect').data(labelArr);
-        const keyLabels = keyGroup.selectAll('text').data(labelArr);
-        keyRects
-          .enter()
-          .append('rect')
-          .attr('width', 15)
-          .attr('height', 15)
-          .attr('fill', (d) => color(d.color))
-          .attr('x', x(xMax) - 25)
-          .attr('y', (_d, i) => y(yMin) - 25 - 20 * i)
-        keyRects
-          .attr('fill', (d) => color(d.color))
-          .attr('x', x(xMax) - 25)
-          .attr('y', (_d, i) => y(yMin) - 25 - 20 * i)
-        keyLabels.enter()
-          .append('text')
-          .attr('text-anchor', 'middle')
-          .attr('alignment-baseline', 'middle')
-          .attr('font-size', 8)
-          .attr('font-weight', 700)
-          .attr('x', x(xMax) - 18)
-          .attr('y', (_d, i) => y(yMin) - 17 - 20 * i)
-          .text(d => d.label);
-        keyLabels
-          .attr('text-anchor', 'middle')
-          .attr('alignment-baseline', 'middle')
-          .attr('font-size', 8)
-          .attr('font-weight', 700)
-          .text(d => d.label)
-          .attr('x', x(xMax) - 18)
-          .attr('y', (_d, i) => y(yMin) - 17 - 20 * i)
-        keyRects.exit().remove();
-        keyLabels.exit().remove();
-
-
         // lines to plot
-        if (!svg.selectAll('g.lines').size()) svg.append('g').attr('class', 'lines');
-        const linesGroup = svg.select('g.lines');
-        const lines = linesGroup.selectAll('polyline').data(
-          linesToPlot);
+        if (!svg.selectAll("g.lines").size())
+          svg.append("g").attr("class", "lines");
+        const linesGroup = svg.select("g.lines");
+        const lines = linesGroup.selectAll("polyline").data(linesToPlot);
         lines
           .enter()
-          .append('polyline')
-          .attr('fill', 'transparent')
-          .attr('stroke', d => color(d.color))
-          .attr('points', (d) =>
-            d.points.map(([a, b]) => `${x(a)},${y(b)}`).join(' ')
-          )
-        lines
-          .attr('stroke', d => color(d.color))
-          .attr('points', (d) =>
-            d.points.map(([a, b]) => `${x(a)},${y(b)}`).join(' ')
-          )
+          .append("polyline")
+          .attr("fill", "transparent")
+          .attr("stroke", (d) => "#666")
+          .attr("stroke-width", 1)
+          .attr("points", (d) =>
+            d.points.map(([a]) => `${x(a)},${y(d.y)}`).join(" ")
+          );
+        lines.attr("points", (d) =>
+          d.points.map(([a]) => `${x(a)},${y(d.y)}`).join(" ")
+        );
         lines.exit().remove();
 
         // points to plot in the form of dots
-        if (!svg.selectAll('g.dots').size()) svg.append('g').attr('class', 'dots');
-        const dotsGroup = svg.select('g.dots');
-        const dots = dotsGroup.selectAll('circle').data(pointsToPlot);
+        if (!svg.selectAll("g.dots").size())
+          svg.append("g").attr("class", "dots");
+        const dotsGroup = svg.select("g.dots");
+        const dots = dotsGroup.selectAll("circle").data(pointsToPlot);
         dots
           .enter()
-          .append('circle')
-          .attr('r', 3)
-          .attr('cx', (d) => x(d.x))
-          .attr('cy', (d) => y(d.y))
-          .attr('fill', (d) => color(d.color))
-          .append('title')
+          .append("circle")
+          .attr("r", 3)
+          .attr("cx", (d) => x(d.x))
+          .attr("cy", (d) => y(d.y))
+          .attr("fill", "#666")
+          .append("title")
           .text((d) => d.label);
         dots
-          .attr('cx', (d) => x(d.x))
-          .attr('cy', (d) => y(d.y))
-          .attr('fill', (d) => color(d.color))
-          .select('title').text((d) => d.label);
+          .attr("cx", (d) => x(d.x))
+          .attr("cy", (d) => y(d.y))
+          .select("title")
+          .text((d) => d.label);
         dots.exit().remove();
 
+        // // points to plot in the form of dots
+        // if (!svg.selectAll("g.points").size())
+        //   svg.append("g").attr("class", "points");
+        // const poitnsGroup = svg.select("g.points");
+        // const points = poitnsGroup.selectAll("circle").data(line1);
+        // points
+        //   .enter()
+        //   .append("circle")
+        //   .attr("r", 3)
+        //   .attr("cx", (d) => x(d))
+        //   .attr("cy", 100)
+        //   .attr("fill", "black")
+        //   .append("title")
+        //   .text((d) => "hello");
+        // points
+        //   .attr("cx", (d) => x(d))
+        //   .attr("cy", 100)
+        //   .attr("fill", "black")
+        //   .select("title")
+        //   .text((d) => "hello");
+        // points.exit().remove();
       }
     },
 
@@ -237,12 +226,12 @@ export const Chart: React.FC<{
      * if the variables are valid, but we no longer need to compare old props to
      * new props to decide wether to re-render.
      */
-    [pointsToPlot, linesToPlot, labelMap]
+    [pointsToPlot, linesToPlot, labelMap, datumsToPlot]
   );
 
   return (
     <svg
-      style={{ border: '1px solid #eee' }}
+      style={{ border: "1px solid #eee" }}
       className="d3-component"
       width="100%"
       height="100%"
